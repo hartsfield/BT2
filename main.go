3335355b1f19fc2e5021568eae59763763506142
hartsfield <johnathanhartsfield@gmail.com>
2023-04-27 11:34:14 -0400

    updates

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func main() {
 	// Server configuration
 	srv := &http.Server{
 		// in production only use SSL
-		Addr:              ":6666",
+		Addr:              ":5555",
 		Handler:           mux,
 		ReadHeaderTimeout: 5 * time.Second,
 		WriteTimeout:      10 * time.Second,
